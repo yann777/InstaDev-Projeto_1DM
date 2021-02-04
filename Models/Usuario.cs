567d2f1d68cfde29af7f258c51e227620028adc3
Yan Oliveira Ferreira <yan12oliveira@gmail.com>
2021-02-04 13:54:47 -0300

    problema upload imagem

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,7 +5,7 @@ using Projeto_InstaDev.Interfaces;
 
 namespace Projeto_InstaDev.Models
 {
-    public class Usuario : InstaDevBase , IEditarPerfil 
+    public class Usuario : InstaDevBase , IEdicao
     {
         public int IdUsuario { get; set; }
         public string Nome { get; set; }
